fb3110096e879443cfa8b9c3cb0de40c902512d8
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2023-10-31 16:55:11 +0200

    UI changes in predefined components of clerk and add favicon

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/nextjs';
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description:
+    'Discover a world of knowledge and expertise at our online community. Join us for answers, discussions, and insights on everything from coding conundrums to technology trends. Unlock the power of collective wisdom at DevFlow',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <ClerkProvider
+        appearance={{
+          elements: {
+            formButtonPrimary: 'primary-gradient',
+            footerActionLink:
+              'primary-text-gradient font-semibold tracking-wide hover:text-black',
+          },
+        }}
+      >
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <main>{children}</main>
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
