41235de6acf4974ddef0fb44d114a87eb14372ac
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2023-11-02 16:29:13 +0200

    Added Routes

--- app/(root)/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(root)/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { SignIn } from '@clerk/nextjs';
+import React from "react";
+import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return <SignIn />;
---
+++ app/(root)/(home)/ask-question/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Ask-questions here</div>;
+};
+
+export default page;
